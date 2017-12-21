6df5db6719eff9b9adc04292ad1d01c3a1738e94
李习靖 <22126153+APlxj@users.noreply.github.com>
2017-12-21 17:49:21 +0800

    一个进程，多个入口

---
+++ mpandroidchart/src/main/java/ap/com/chart/TwoActivity.java
@@ -0,0 +1,26 @@
+package ap.com.chart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * 类描述：
+ * 创建人：swallow.li
+ * 创建时间：
+ * Email: swallow.li@kemai.cn
+ * 修改备注：
+ */
+public class TwoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
